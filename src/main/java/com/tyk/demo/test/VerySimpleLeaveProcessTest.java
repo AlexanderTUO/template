f17754349e917e8a88376dbc8c5e4fb533a0237b
tuoyangkun <1599961496@qq.com>
2019-08-26 17:29:22 +0800

    activiti流程

--- src/main/java/com/tyk/demo/test/VerySimpleLeaveProcessTest.java
+++ src/main/java/com/tyk/demo/test/VerySimpleLeaveProcessTest.java
@@ -4,12 +4,16 @@ import com.tyk.demo.DemoApplication;
 import org.activiti.engine.ProcessEngine;
 import org.activiti.engine.ProcessEngineConfiguration;
 import org.activiti.engine.RepositoryService;
+import org.activiti.engine.RuntimeService;
+import org.activiti.engine.repository.ProcessDefinition;
+import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @Author: tyk
@@ -26,14 +30,26 @@ public class VerySimpleLeaveProcessTest {
                 .createStandaloneProcessEngineConfiguration()
                 .buildProcessEngine();
 
+        //部署
         RepositoryService repositoryService = processEngine.getRepositoryService();
         String bpmnFileName = "/diagram/leaveProcess.bpmn";
-//        repositoryService.createDeployment()
-//                .addInputStream("sayHelloToLeave", new FileInputStream(bpmnFileName));
-        //部署
+        repositoryService.createDeployment()
+                .addInputStream("sayHelloToLeave",
+                        this.getClass().getClassLoader().getResourceAsStream(bpmnFileName))
+                .deploy();
 
-        //验证
+        ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().singleResult();
+        System.out.println("流程Key：" + processDefinition.getKey());
 
+        //定义运行时，开启流程实例
+        RuntimeService runtimeService = processEngine.getRuntimeService();
+        ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("sayHelloToLeave");
+
+        System.out.println("pid:" + processInstance.getId() + ",pdid:" + processInstance.getProcessDefinitionId());
+
+        Map<String, String> variables = new HashMap<>();
+        variables.put("applyUser", "tyk");
+        variables.put("days", "3");
         //启动流程
     }
 }
